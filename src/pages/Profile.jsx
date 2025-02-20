6e1a627c5fcf4acce52e112f8e3beede2a590c0a
Kaushik <kaushiknr2002@gmail.com>
2025-02-20 12:23:35 +0530

    create pages and router

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+
+export default function Profile() {
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element="<Home />"></Route>
+            <Route path="/About" element="<About />"></Route>
+            <Route path="/Profile" element="<Profile />"></Route>
+            <Route path="/Sign-in" element="<Signin />"></Route>
+            <Route path="/Sign-up" element="<Sign-up />"></Route>
+        </Routes>
+    </BrowserRouter>
+   
+    
+  )
+}
